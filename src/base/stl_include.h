6e147767776906934cfb0e42dac5a10ae4db0dca
Dreamer Dead <dreamer.dead@gmail.com>
2016-03-28 20:21:20 +0300

    Hack to fix windows build.

--- src/base/stl_include.h
+++ src/base/stl_include.h
@@ -5,7 +5,7 @@
 #else
 #if !__has_feature(cxx_exceptions)
 #define _HAS_EXCEPTIONS 0
-#define __uncaught_exception std::uncaught_exception
+//#define __uncaught_exception std::uncaught_exception
 #endif  // !__has_feature(cxx_exceptions)
 #define STL(file_name) <file_name>
 #endif  // !defined(OS_WIN)
